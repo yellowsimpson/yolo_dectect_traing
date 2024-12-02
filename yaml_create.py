9e09a72b39747e18816e6edac72a3e3ec9053326
yellowsimpson <shimsungwhan@naver.com>
2024-12-02 09:52:50 +0900

    tain4 update

--- yaml_create.py
+++ yaml_create.py
@@ -1,16 +1,16 @@
 import yaml
 
 data = {
-    "train" : '/box_data_2/train/',
-        "val" : '/box_data_2/valid/',
-        "test" : '/box_data_2/test/', 
+    "train" : '/box_data_3/train/',
+        "val" : '/box_data_3/valid/',
+        "test" : '/box_data_3/test/', 
         "names" : {0 : 'box'}}
 
-with open('./box_data_2.yaml', 'w') as f :
+with open('./box_data_3.yaml', 'w') as f :
     yaml.dump(data, f)
 
 # check written file
-with open('./box_data_2.yaml', 'r') as f :
+with open('./box_data_3.yaml', 'r') as f :
     lines = yaml.safe_load(f)
     print(lines)
     
