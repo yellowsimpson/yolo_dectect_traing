a5007f720a36aea5e7fef90b513265b52bcbb7be
yellowsimpson <shimsungwhan@naver.com>
2024-12-03 18:26:18 +0900

    train 6 update

--- yaml_create.py
+++ yaml_create.py
@@ -1,16 +1,16 @@
 import yaml
 
 data = {
-    "train" : '/box_data_4/train/',
-        "val" : '/box_data_4/valid/',
-        "test" : '/box_data_4/test/', 
-        "names" : {0 : 'box'}}
+    "train" : '/box_data_6/train/',
+        "val" : '/box_data_6/valid/',
+        "test" : '/box_data_6/test/', 
+        "names" : {0 : 'red_line'}}
 
-with open('./box_data_4.yaml', 'w') as f :
+with open('./box_data_6.yaml', 'w') as f :
     yaml.dump(data, f)
 
 # check written file
-with open('./box_data_4.yaml', 'r') as f :
+with open('./box_data_6.yaml', 'r') as f :
     lines = yaml.safe_load(f)
     print(lines)
     
